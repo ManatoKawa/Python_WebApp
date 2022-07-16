e5a3f065fcdf75e23bd2e1e488f69166f13a9587
Manato Kawa <2004240008km@edu.oca.ac.jp>
2022-07-16 17:28:10 +0900

    ss

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, render_template
 from flask import request
-
+from flask_sqlalchemy import SQLAlchemy
+from mysql_model import Person
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:p%40ssw0rd1@mysqldb/test_mysql?charset=utf8mb4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 @app.route('/')
 def inex():
     return 'Response Data'
@@ -30,3 +35,14 @@ def show_data():
 @app.route('/try_css')
 def try_css():
     return render_template('./css.css')
+
+@app.route('/person_search')
+def person_search():
+    return render_template('./person_search.html')
+
+
+@app.route('/person_result')
+def person_result():
+    search_size = request.args.get("search_size")
+    persons = db.session.query(Person).filter(Person.size > search_size)
+    return render_template('./person_result.html', persons=persons, search_size=search_size)
